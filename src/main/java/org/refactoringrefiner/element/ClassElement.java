3d18e15d74f741382329cfc6cde41b14eacd4fb8
Mehran Jodavi <mehran.jodavi@gmail.com>
2020-05-30 17:04:04 -0400

    some methods are changed in ClassElement

--- src/main/java/org/refactoringrefiner/element/ClassElement.java
+++ src/main/java/org/refactoringrefiner/element/ClassElement.java
@@ -1,13 +1,21 @@
 package org.refactoringrefiner.element;
 
 import gr.uom.java.xmi.UMLClass;
+import gr.uom.java.xmi.diff.MoveAndRenameClassRefactoring;
+import gr.uom.java.xmi.diff.MoveClassRefactoring;
+import gr.uom.java.xmi.diff.RenameClassRefactoring;
 import org.apache.commons.lang3.builder.EqualsBuilder;
 import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.refactoringminer.api.Refactoring;
 import org.refactoringrefiner.api.Version;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
 
 public class ClassElement extends BaseCodeElement {
 
-    private final UMLClass info;
+    protected final UMLClass info;
 
     public ClassElement(UMLClass info, Version version) {
         super(version);
@@ -24,7 +32,8 @@ public class ClassElement extends BaseCodeElement {
                 .append(this.info.getPackageName(), that.info.getPackageName())
                 .append(this.info.getName(), that.info.getName())
                 .append(this.getVersion().getId(), that.getVersion().getId())
-                .isEquals();
+                .isEquals()
+                && new HashSet<>(this.info.getAnnotations()).containsAll(new HashSet<>(that.info.getAnnotations()));
     }
 
     @Override
@@ -39,20 +48,24 @@ public class ClassElement extends BaseCodeElement {
 
     @Override
     public String toString() {
-        return getFullName();
+        return this.getFullName();
     }
 
     @Override
     public String getIdentifier() {
         StringBuilder sb = new StringBuilder();
-        sb.append(getFullName());
+        sb.append(this.getSourceFolder());
+        sb.append(this.info.getName());
         sb.append(this.getVersion().toString());
         return sb.toString();
     }
 
     @Override
     public String getFullName() {
-        return getPath(this.info.getLocationInfo().getFilePath(), this.info.getName()) + info.toString();
+        StringBuilder sb = new StringBuilder();
+        sb.append(this.getSourceFolder());
+        sb.append(this.info.getName());
+        return sb.toString();
     }
 
     @Override
