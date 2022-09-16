a28939a055a26b17a23ab734cae38cf8af84c0ee
tsantalis <tsantalis@gmail.com>
2022-09-16 12:38:41 -0400

    Initial BlockTracker implementation

--- src/main/java/org/codetracker/BlockTrackerImpl.java
+++ src/main/java/org/codetracker/BlockTrackerImpl.java
@@ -1,12 +1,17 @@
 package org.codetracker;
 
 import gr.uom.java.xmi.LocationInfo.CodeElementType;
-import org.codetracker.BaseTracker;
-import org.codetracker.ChangeHistory;
+import gr.uom.java.xmi.UMLModel;
 import org.codetracker.api.BlockTracker;
+import org.codetracker.api.CodeElementNotFoundException;
 import org.codetracker.api.History;
+import org.codetracker.api.Version;
 import org.codetracker.element.Block;
+import org.codetracker.element.Method;
+import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Repository;
+
+import java.util.Collections;
 
 public class BlockTrackerImpl extends BaseTracker implements BlockTracker {
     private final ChangeHistory<Block> blockChangeHistory = new ChangeHistory<>();
@@ -27,8 +32,36 @@ public class BlockTrackerImpl extends BaseTracker implements BlockTracker {
         this.blockEndLineNumber = blockEndLineNumber;
     }
 
+    private boolean isStartBlock(Block block) {
+        return block.getComposite().getLocationInfo().getCodeElementType().equals(blockType) &&
+                block.getComposite().getLocationInfo().getStartLine() == blockStartLineNumber &&
+                block.getComposite().getLocationInfo().getEndLine() == blockEndLineNumber;
+    }
+
+    private boolean isStartMethod(Method method) {
+        return method.getUmlOperation().getName().equals(methodName) &&
+                method.getUmlOperation().getLocationInfo().getStartLine() <= methodDeclarationLineNumber &&
+                method.getUmlOperation().getLocationInfo().getEndLine() >= methodDeclarationLineNumber;
+    }
+
     @Override
     public History<Block> track() throws Exception {
-        return null;
+        HistoryImpl.HistoryReportImpl historyReport = new HistoryImpl.HistoryReportImpl();
+        try (Git git = new Git(repository)) {
+            Version startVersion = gitRepository.getVersion(startCommitId);
+            UMLModel umlModel = getUMLModel(startCommitId, Collections.singleton(filePath));
+            Method startMethod = getMethod(umlModel, startVersion, this::isStartMethod);
+            if (startMethod == null) {
+                throw new CodeElementNotFoundException(filePath, methodName, methodDeclarationLineNumber);
+            }
+            Block startBlock = startMethod.findBlock(this::isStartBlock);
+            if (startBlock == null) {
+                throw new CodeElementNotFoundException(filePath, blockType.getName(), blockStartLineNumber);
+            }
+            blockChangeHistory.addNode(startBlock);
+
+
+            return new HistoryImpl<>(blockChangeHistory.findSubGraph(startBlock), historyReport);
+        }
     }
 }
