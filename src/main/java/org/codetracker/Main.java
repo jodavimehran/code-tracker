ead30715e5a1b945831399ce2b54820387cbb12e
Tayeeb Hasan <tayeeb.hasan@gmail.com>
2022-11-28 14:40:51 -0500

    add block tracking example

--- src/main/java/org/codetracker/Main.java
+++ src/main/java/org/codetracker/Main.java
@@ -1,8 +1,10 @@
 package org.codetracker;
 
+import gr.uom.java.xmi.LocationInfo;
 import org.codetracker.api.*;
 import org.codetracker.change.Change;
 import org.codetracker.element.Attribute;
+import org.codetracker.element.Block;
 import org.codetracker.element.Method;
 import org.codetracker.element.Variable;
 import org.eclipse.jgit.lib.Repository;
@@ -19,7 +21,7 @@ public class Main {
         GitService gitService = new GitServiceImpl();
         // METHOD TRACKING EXAMPLE
         try (Repository repository = gitService.cloneIfNotExists(FOLDER_TO_CLONE + "checkstyle\\checkstyle",
-                "https://github.com/checkstyle/checkstyle.git")){
+                "https://github.com/checkstyle/checkstyle.git")) {
 
             MethodTracker methodTracker = CodeTracker.methodTracker()
                     .repository(repository)
@@ -45,9 +47,10 @@ public class Main {
             }
             System.out.println("======================================================");
         }
+
         // VARIABLE TRACKING EXAMPLE
         try (Repository repository = gitService.cloneIfNotExists(FOLDER_TO_CLONE + "checkstyle\\checkstyle",
-                "https://github.com/checkstyle/checkstyle.git")){
+                "https://github.com/checkstyle/checkstyle.git")) {
 
             VariableTracker variableTracker = CodeTracker.variableTracker()
                     .repository(repository)
@@ -91,6 +94,38 @@ public class Main {
             History<Attribute> attributeHistory = attributeTracker.track();
 
             for (History.HistoryInfo<Attribute> historyInfo : attributeHistory.getHistoryInfoList()) {
+                System.out.println("======================================================");
+                System.out.println("Commit ID: " + historyInfo.getCommitId());
+                System.out.println("Date: " +
+                        LocalDateTime.ofEpochSecond(historyInfo.getCommitTime(), 0, ZoneOffset.UTC));
+                System.out.println("Before: " + historyInfo.getElementBefore().getName());
+                System.out.println("After: " + historyInfo.getElementAfter().getName());
+
+                for (Change change : historyInfo.getChangeList()) {
+                    System.out.println(change.getType().getTitle() + ": " + change);
+                }
+            }
+            System.out.println("======================================================");
+        }
+
+        // BLOCK TRACKING EXAMPLE
+        try (Repository repository = gitService.cloneIfNotExists(FOLDER_TO_CLONE + "checkstyle\\checkstyle",
+                "https://github.com/checkstyle/checkstyle.git")) {
+
+            BlockTracker blockTracker = CodeTracker.blockTracker()
+                    .repository(repository)
+                    .filePath("src/main/java/com/puppycrawl/tools/checkstyle/Checker.java")
+                    .startCommitId("119fd4fb33bef9f5c66fc950396669af842c21a3")
+                    .methodName("setupChild")
+                    .methodDeclarationLineNumber(448)
+                    .codeElementType(LocationInfo.CodeElementType.TRY_STATEMENT)
+                    .blockStartLineNumber(453)
+                    .blockEndLineNumber(465)
+                    .build();
+
+            History<Block> blockHistory = blockTracker.track();
+
+            for (History.HistoryInfo<Block> historyInfo : blockHistory.getHistoryInfoList()) {
                 System.out.println("======================================================");
                 System.out.println("Commit ID: " + historyInfo.getCommitId());
                 System.out.println("Date: " +
