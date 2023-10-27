770f4219e8c306d31e927077f5c8c1811c82e0ac
tsantalis <tsantalis@gmail.com>
2023-10-27 09:56:25 -0400

    Added block tracking example in Main

--- src/main/java/org/codetracker/Main.java
+++ src/main/java/org/codetracker/Main.java
@@ -3,11 +3,14 @@ package org.codetracker;
 import org.codetracker.api.*;
 import org.codetracker.change.Change;
 import org.codetracker.element.Attribute;
+import org.codetracker.element.Block;
 import org.codetracker.element.Method;
 import org.codetracker.element.Variable;
 import org.eclipse.jgit.lib.Repository;
 import org.refactoringminer.api.GitService;
 import org.refactoringminer.util.GitServiceImpl;
+
+import gr.uom.java.xmi.LocationInfo.CodeElementType;
 
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
@@ -17,6 +20,37 @@ public class Main {
 
     public static void main(String args[]) throws Exception {
         GitService gitService = new GitServiceImpl();
+        // BLOCK TRACKING EXAMPLE
+        try (Repository repository = gitService.cloneIfNotExists(FOLDER_TO_CLONE + "checkstyle\\checkstyle",
+                "https://github.com/checkstyle/checkstyle.git")){
+
+            BlockTracker blockTracker = CodeTracker.blockTracker()
+                    .repository(repository)
+                    .filePath("src/main/java/com/puppycrawl/tools/checkstyle/Checker.java")
+                    .startCommitId("119fd4fb33bef9f5c66fc950396669af842c21a3")
+                    .methodName("fireErrors")
+                    .methodDeclarationLineNumber(384)
+                    .codeElementType(CodeElementType.ENHANCED_FOR_STATEMENT)
+                    .blockStartLineNumber(391)
+                    .blockEndLineNumber(393)
+                    .build();
+
+            History<Block> blockHistory = blockTracker.track();
+
+            for (History.HistoryInfo<Block> historyInfo : blockHistory.getHistoryInfoList()) {
+                System.out.println("======================================================");
+                System.out.println("Commit ID: " + historyInfo.getCommitId());
+                System.out.println("Date: " +
+                        LocalDateTime.ofEpochSecond(historyInfo.getCommitTime(), 0, ZoneOffset.UTC));
+                System.out.println("Before: " + historyInfo.getElementBefore().getName());
+                System.out.println("After: " + historyInfo.getElementAfter().getName());
+
+                for (Change change : historyInfo.getChangeList()) {
+                    System.out.println(change.getType().getTitle() + ": " + change);
+                }
+            }
+            System.out.println("======================================================");
+        }
         // METHOD TRACKING EXAMPLE
         try (Repository repository = gitService.cloneIfNotExists(FOLDER_TO_CLONE + "checkstyle\\checkstyle",
                 "https://github.com/checkstyle/checkstyle.git")){
