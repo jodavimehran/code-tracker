8199fd81b7aaab27e701b0e00faa38241d8d224b
Pouryafard75 <pouryafard75@gmail.com>
2024-03-25 12:51:35 -0400

    Experiment with the workflow

--- src/test/java/org/codetracker/util/CodeElementLocatorTest.java
+++ src/test/java/org/codetracker/util/CodeElementLocatorTest.java
@@ -1,20 +1,30 @@
 package org.codetracker.util;
 
+import com.opencsv.CSVReader;
+import com.opencsv.exceptions.CsvException;
 import org.codetracker.api.CodeElement;
 import org.codetracker.element.Attribute;
 import org.codetracker.element.Block;
 import org.codetracker.element.Method;
 import org.codetracker.element.Variable;
+import org.codetracker.experiment.AbstractExperimentStarter;
+import org.codetracker.experiment.MethodExperimentStarter;
 import org.eclipse.jgit.lib.Repository;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.refactoringminer.api.GitService;
 import org.refactoringminer.util.GitServiceImpl;
+
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.List;
+import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 
-@Disabled
 public class CodeElementLocatorTest {
     private final static String FOLDER_TO_CLONE = "tmp/";
 
@@ -506,6 +516,60 @@ public class CodeElementLocatorTest {
             assertNotNull(codeElement);
             assertEquals(codeElement.getClass(), Block.class);
             assertEquals(codeElement.getLocation().getStartLine(), lineNumber);
+        }
+    }
+
+
+
+    private static final String generatedFilePathTemplate = "experiments/tracking-accuracy/%s/tracker/final.csv";
+    private static final String expectedFilePathTemplate = "src/test/resources/tracking-accuracy/%s/tracker/final.csv";
+
+    public static Stream<Arguments> experimentStartedProvider() {
+        return Stream.of(
+//                Arguments.of(new AttributeExperimentStarter(), "attribute"),
+                Arguments.of(new MethodExperimentStarter(), "method")
+//                Arguments.of(new ClassExperimentStarter(), "class"),
+//                Arguments.of(new VariableExperimentStarter(), "variable"),
+//                Arguments.of(new BlockExperimentStarter(), "block")
+        );
+    }
+
+    @Test
+    public void c(){
+        System.out.println("a");
+    }
+
+    @ParameterizedTest(name = "{index}: {0}")
+    @MethodSource(value = "experimentStartedProvider")
+    public void ExperimentStarterTest(AbstractExperimentStarter abstractExperimentStarter, String folderName) throws IOException, CsvException {
+        String expectedFilePath = String.format(expectedFilePathTemplate, folderName);
+        String generatedFilePath = String.format(generatedFilePathTemplate, folderName);
+        System.out.println(expectedFilePath);
+        System.out.println(generatedFilePath);
+//        abstractExperimentStarter.start();
+        try (
+                CSVReader expectedCSV = new CSVReader(new FileReader(expectedFilePath));
+                CSVReader actualCSV = new CSVReader(new FileReader(generatedFilePath))
+        ) {
+            List<String[]> expectedLines = expectedCSV.readAll();
+            List<String[]> actualLines = actualCSV.readAll();
+
+            for (int i = 0; i < actualLines.size() && i < expectedLines.size(); i++) {
+                String[] record1 = actualLines.get(i);
+                String[] record2 = expectedLines.get(i);
+                String cellCsv1_col6 = record1[6];
+                String cellCsv1_col7 = record1[7];
+                String cellCsv1_col8 = record1[8];
+                String cellCsv2_col6 = record2[6];
+                String cellCsv2_col7 = record2[7];
+                String cellCsv2_col8 = record2[8];
+                System.out.println("Comparing " + cellCsv1_col6 + " with " + cellCsv2_col6);
+                System.out.println("Comparing " + cellCsv1_col7 + " with " + cellCsv2_col8);
+                System.out.println("Comparing " + cellCsv1_col7 + " with " + cellCsv2_col8);
+                assertEquals(cellCsv1_col6, cellCsv2_col6);
+                assertEquals(cellCsv1_col7, cellCsv2_col7);
+                assertEquals(cellCsv1_col8, cellCsv2_col8);
+            }
         }
     }
 }
