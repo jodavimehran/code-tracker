8a9ae25744692901bbe75b30e5487b6a4ddc248d
Pouryafard75 <pouryafard75@gmail.com>
2024-04-02 02:28:13 -0400

    Parameterize OracleTest

--- src/test/java/org/codetracker/util/BlockOracleTest.java
+++ src/test/java/org/codetracker/util/BlockOracleTest.java
@@ -1,8 +1,6 @@
 package org.codetracker.util;
 
-import java.io.IOException;
-import java.util.List;
-
+import gr.uom.java.xmi.LocationInfo;
 import org.codetracker.api.BlockTracker;
 import org.codetracker.api.CodeTracker;
 import org.codetracker.api.History;
@@ -10,14 +8,15 @@ import org.codetracker.element.Block;
 import org.codetracker.experiment.oracle.BlockOracle;
 import org.codetracker.experiment.oracle.history.BlockHistoryInfo;
 import org.eclipse.jgit.lib.Repository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
 
-import gr.uom.java.xmi.LocationInfo.CodeElementType;;
+import java.io.IOException;
+import java.util.stream.Stream;
 
 public class BlockOracleTest extends OracleTest {
 	private static final String EXPECTED = System.getProperty("user.dir") + "/src/test/resources/block/";
 
-	private History<Block> blockTracker(BlockHistoryInfo blockHistoryInfo, Repository repository) throws Exception {
+	private static History<Block> blockTracker(BlockHistoryInfo blockHistoryInfo, Repository repository) throws Exception {
 		BlockTracker blockTracker = CodeTracker
 			.blockTracker()
 			.repository(repository)
@@ -25,19 +24,14 @@ public class BlockOracleTest extends OracleTest {
 			.startCommitId(blockHistoryInfo.getStartCommitId())
 			.methodName(blockHistoryInfo.getFunctionName())
 			.methodDeclarationLineNumber(blockHistoryInfo.getFunctionStartLine())
-			.codeElementType(CodeElementType.valueOf(blockHistoryInfo.getBlockType()))
+			.codeElementType(LocationInfo.CodeElementType.valueOf(blockHistoryInfo.getBlockType()))
 			.blockStartLineNumber(blockHistoryInfo.getBlockStartLine())
 			.blockEndLineNumber(blockHistoryInfo.getBlockEndLine())
 			.build();
 		return blockTracker.track();
 	}
 
-	@Test
-	public void testAccuracy() throws IOException, InterruptedException {
-		List<BlockOracle> oracles = BlockOracle.all();
-		for (BlockOracle oracle : oracles) {
-			loadExpected(EXPECTED + oracle.getName() + "-expected.txt");
-			codeTracker(oracle, this::blockTracker, ALL_CORES);
-		}
+	public static Stream<Arguments> testProvider() throws IOException {
+		return getArgumentsStream(BlockOracle.all(), EXPECTED, BlockOracleTest::blockTracker);
 	}
 }
--- src/test/java/org/codetracker/util/VariableOracleTest.java
+++ src/test/java/org/codetracker/util/VariableOracleTest.java
@@ -1,8 +1,5 @@
 package org.codetracker.util;
 
-import java.io.IOException;
-import java.util.List;
-
 import org.codetracker.api.CodeTracker;
 import org.codetracker.api.History;
 import org.codetracker.api.VariableTracker;
@@ -10,12 +7,15 @@ import org.codetracker.element.Variable;
 import org.codetracker.experiment.oracle.VariableOracle;
 import org.codetracker.experiment.oracle.history.VariableHistoryInfo;
 import org.eclipse.jgit.lib.Repository;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.provider.Arguments;
+
+import java.io.IOException;
+import java.util.stream.Stream;
 
 public class VariableOracleTest extends OracleTest {
 	private static final String EXPECTED = System.getProperty("user.dir") + "/src/test/resources/variable/";
 
-    private History<Variable> variableTracker(VariableHistoryInfo variableHistoryInfo, Repository repository) throws Exception {
+    private static History<Variable> variableTracker(VariableHistoryInfo variableHistoryInfo, Repository repository) throws Exception {
         VariableTracker variableTracker = CodeTracker.variableTracker()
             .repository(repository)
             .filePath(variableHistoryInfo.getFilePath())
@@ -28,12 +28,7 @@ public class VariableOracleTest extends OracleTest {
         return variableTracker.track();
     }
 
-	@Test
-	public void testAccuracy() throws IOException, InterruptedException {
-		List<VariableOracle> oracles = VariableOracle.all();
-		for (VariableOracle oracle : oracles) {
-			loadExpected(EXPECTED + oracle.getName() + "-expected.txt");
-			codeTracker(oracle, this::variableTracker, HALF_CORES);
-		}
+	public static Stream<Arguments> testProvider() throws IOException {
+		return getArgumentsStream(VariableOracle.all(), EXPECTED, VariableOracleTest::variableTracker);
 	}
 }
