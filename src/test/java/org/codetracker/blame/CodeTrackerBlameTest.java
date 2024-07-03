da4200975e6c71613cf507452f85f82f2528aba3
Pouryafard75 <pouryafard75@gmail.com>
2024-07-03 10:23:31 -0400

    Add new tests

--- src/test/java/org/codetracker/blame/CodeTrackerBlameTest.java
+++ src/test/java/org/codetracker/blame/CodeTrackerBlameTest.java
@@ -45,8 +45,6 @@ public class CodeTrackerBlameTest {
                 Arguments.of(url, filePath, new GitBlame(), "/blame/formatting/git.txt")
         );
     }
-
-
     @Test
     public void testBlameWithLocalRepo() throws Exception {
         String url = "https://github.com/pouryafard75/DiffBenchmark/commit/5b33dc6f8cfcf8c0e31966c035b0406eca97ec76";
@@ -79,5 +77,36 @@ public class CodeTrackerBlameTest {
         LineBlameResult lineBlameResult = LineBlameResult.of(lineBlame);
         Assertions.assertEquals(expected, lineBlameResult.toString());
     }
+
+    @Test
+    public void blameTestSingleLine2() throws Exception {
+        String url = "https://github.com/pouryafard75/DiffBenchmark/commit/5b33dc6f8cfcf8c0e31966c035b0406eca97ec76";
+        String filePath = "src/main/java/dat/MakeIntels.java";
+        int lineNumber = 24;
+        String expected = "LineBlameResult{commitId='ae3a4f8a50b1d25b2d3db50495a50feaa0b2b872', filePath='src/main/java/dat/MakeIntels.java', shortCommitId='ae3a4f8a5', beforeFilePath='src/main/java/dat/Make.java', committer='Pouryafard75', commitDate='1719280029', lineNumber=18}";
+
+        String commitId = URLHelper.getCommit(url);
+        Repository repository = gitService.cloneIfNotExists(REPOS_PATH + "/" + getProject(url), URLHelper.getRepo(url));
+        History.HistoryInfo<? extends CodeElement> lineBlame =
+                new CodeTrackerBlame().getLineBlame(repository, commitId, filePath, lineNumber);
+        LineBlameResult lineBlameResult = LineBlameResult.of(lineBlame);
+//        Assertions.assertEquals(expected, lineBlameResult.toString()); //TODO: Update this one
+    }
+
+    @Test
+    public void blameTestSingleLine_3() throws Exception {
+        String url = "https://github.com/Alluxio/alluxio/commit/9aeefcd8120bb3b89cdb437d8c32d2ed84b8a825";
+        String filePath = "servers/src/main/java/tachyon/worker/block/allocator/MaxFreeAllocator.java";
+        int lineNumber = 78;
+        String expected = "???";
+
+        String commitId = URLHelper.getCommit(url);
+        Repository repository = gitService.cloneIfNotExists(REPOS_PATH + "/" + getProject(url), URLHelper.getRepo(url));
+        History.HistoryInfo<? extends CodeElement> lineBlame =
+                new CodeTrackerBlame().getLineBlame(repository, commitId, filePath, lineNumber);
+        LineBlameResult lineBlameResult = LineBlameResult.of(lineBlame);
+//        Assertions.assertEquals(expected, lineBlameResult.toString()); //TODO: Update this one
+    }
+
 
 }
