720672d4d2d11c5fbed9f5c1caa1b7940b38c56d
tsantalis <tsantalis@gmail.com>
2024-07-16 07:42:11 -0400

    Add exception handling for locateWithoutName

--- src/main/java/org/codetracker/FileTrackerImpl.java
+++ src/main/java/org/codetracker/FileTrackerImpl.java
@@ -11,6 +11,7 @@ import java.util.Map;
 import java.util.Set;
 
 import org.codetracker.api.CodeElement;
+import org.codetracker.api.CodeElementNotFoundException;
 import org.codetracker.api.Version;
 import org.codetracker.util.CodeElementLocator;
 import org.codetracker.util.GitRepository;
@@ -30,31 +31,35 @@ public class FileTrackerImpl extends BaseTracker {
 
 	public void blame() throws Exception {
 		try (Git git = new Git(repository); RevWalk walk = new RevWalk(repository)) {
-            Version startVersion = gitRepository.getVersion(startCommitId);
-            
-            RevCommit revCommit = walk.parseCommit(repository.resolve(startCommitId));
-            Set<String> repositoryDirectories = new LinkedHashSet<>();
-            Map<String, String> fileContents = new LinkedHashMap<>();
-            GitHistoryRefactoringMinerImpl.populateFileContents(repository, revCommit, Collections.singleton(filePath), fileContents, repositoryDirectories);
-            UMLModel umlModel = GitHistoryRefactoringMinerImpl.createModel(fileContents, repositoryDirectories);
-            umlModel.setPartial(true);
-            // extract program elements to be blamed
-            String fileContentAsString = fileContents.get(filePath);
-            List<String> lines = new ArrayList<>();
-            Map<Integer, CodeElement> lineNumberToCodeElementMap = new LinkedHashMap<>();
-            Set<CodeElement> programElements = new LinkedHashSet<>();
-            try (BufferedReader reader = new BufferedReader(new StringReader(fileContentAsString))) {
-                String line;
-                int lineNumber = 1;
-                while ((line = reader.readLine()) != null) {
-                    lines.add(line);
-                    CodeElementLocator locator = new CodeElementLocator((GitRepository) gitRepository, startCommitId, filePath, lineNumber);
-                    CodeElement codeElement = locator.locateWithoutName(startVersion, umlModel);
-                    programElements.add(codeElement);
-                    lineNumberToCodeElementMap.put(lineNumber, codeElement);
-                    lineNumber++;
-                }
-            }
+			Version startVersion = gitRepository.getVersion(startCommitId);
+			RevCommit revCommit = walk.parseCommit(repository.resolve(startCommitId));
+			Set<String> repositoryDirectories = new LinkedHashSet<>();
+			Map<String, String> fileContents = new LinkedHashMap<>();
+			GitHistoryRefactoringMinerImpl.populateFileContents(repository, revCommit, Collections.singleton(filePath), fileContents, repositoryDirectories);
+			UMLModel umlModel = GitHistoryRefactoringMinerImpl.createModel(fileContents, repositoryDirectories);
+			umlModel.setPartial(true);
+			// extract program elements to be blamed
+			String fileContentAsString = fileContents.get(filePath);
+			List<String> lines = new ArrayList<>();
+			Map<Integer, CodeElement> lineNumberToCodeElementMap = new LinkedHashMap<>();
+			Set<CodeElement> programElements = new LinkedHashSet<>();
+			try (BufferedReader reader = new BufferedReader(new StringReader(fileContentAsString))) {
+				String line;
+				int lineNumber = 1;
+				while ((line = reader.readLine()) != null) {
+					lines.add(line);
+					CodeElementLocator locator = new CodeElementLocator((GitRepository) gitRepository, startCommitId, filePath, lineNumber);
+					CodeElement codeElement = null;
+					try {
+						codeElement = locator.locateWithoutName(startVersion, umlModel);
+					} catch (CodeElementNotFoundException e) {}
+					if (codeElement != null) {
+						programElements.add(codeElement);
+						lineNumberToCodeElementMap.put(lineNumber, codeElement);
+					}
+					lineNumber++;
+				}
+			}
 		}
 	}
 }
