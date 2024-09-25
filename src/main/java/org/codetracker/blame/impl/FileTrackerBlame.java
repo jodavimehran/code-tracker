e1cee7684d800392a2a18f1099b630133b24a7b1
Pouryafard75 <pouryafard75@gmail.com>
2024-09-25 13:44:03 -0400

    FileTrackerBlame revisited

--- src/main/java/org/codetracker/blame/impl/FileTrackerBlame.java
+++ src/main/java/org/codetracker/blame/impl/FileTrackerBlame.java
@@ -18,22 +18,7 @@ public class FileTrackerBlame implements IBlame {
     private final static Logger logger = LoggerFactory.getLogger(FileTrackerBlame.class);
     @Override
     public List<LineBlameResult> blameFile(Repository repository, String commitId, String filePath) throws Exception {
-        FileTrackerImpl fileTracker = new FileTrackerImpl(repository, commitId, filePath);
-        fileTracker.blame();
-        List<LineBlameResult> result = new ArrayList<>();
-        for (Map.Entry<Integer, History.HistoryInfo<? extends BaseCodeElement>> entry : fileTracker.getBlameInfo().entrySet()) {
-            Integer lineNumber = entry.getKey();
-            History.HistoryInfo<? extends BaseCodeElement> value = entry.getValue();
-            try {
-                if (fileTracker.getLines().get(lineNumber- 1).isBlank())
-                    result.add(null);
-                else
-                    result.add(LineBlameResult.of(value, lineNumber));
-            } catch (Exception e) {
-                logger.error(e.getMessage());
-            }
-        }
-        return result;
+        return this.blameFile(repository, commitId, filePath, 1, Integer.MAX_VALUE);
     }
 
     @Override
@@ -41,22 +26,20 @@ public class FileTrackerBlame implements IBlame {
         FileTrackerImpl fileTracker = new FileTrackerImpl(repository, commitId, filePath);
         fileTracker.blame();
         List<LineBlameResult> result = new ArrayList<>();
-        for (Map.Entry<Integer, History.HistoryInfo<? extends BaseCodeElement>> entry : fileTracker.getBlameInfo().entrySet()) {
-            Integer lineNumber = entry.getKey();
-            History.HistoryInfo<? extends BaseCodeElement> value = entry.getValue();
-            if (lineNumber < fromLine || lineNumber > toLine) {
-                continue;
-            }
+        if (fromLine < 0) {
+            fromLine = 1;
+        }
+        if (toLine > fileTracker.getLines().size()) {
+            toLine = fileTracker.getLines().size();
+        }
+        for (int lineNumber = fromLine; lineNumber <= toLine; lineNumber++) {
+            History.HistoryInfo<? extends BaseCodeElement> value = fileTracker.getBlameInfo().get(lineNumber);
             try {
-                if (fileTracker.getLines().get(lineNumber- 1).isBlank())
-                    result.add(null);
-                else
-                    result.add(LineBlameResult.of(value, lineNumber));
+                result.add(LineBlameResult.of(value, lineNumber));
             } catch (Exception e) {
                 logger.error(e.getMessage());
             }
         }
         return result;
-
     }
 }
