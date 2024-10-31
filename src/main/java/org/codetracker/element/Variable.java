51187d0ff459dd4c75a66e8745521ae88fadb7df
tsantalis <tsantalis@gmail.com>
2024-10-31 10:39:17 -0400

    Improve variable signature generation to guarantee a unique signature for lambda parameters

--- src/main/java/org/codetracker/element/Variable.java
+++ src/main/java/org/codetracker/element/Variable.java
@@ -3,6 +3,7 @@ package org.codetracker.element;
 import gr.uom.java.xmi.LocationInfo;
 import gr.uom.java.xmi.VariableDeclarationContainer;
 import gr.uom.java.xmi.decomposition.AbstractCodeFragment;
+import gr.uom.java.xmi.decomposition.LambdaExpressionObject;
 import gr.uom.java.xmi.decomposition.VariableDeclaration;
 import org.codetracker.api.Version;
 import org.codetracker.util.Util;
@@ -29,9 +30,40 @@ public class Variable extends BaseCodeElement {
     }
 
     public static Variable of(VariableDeclaration variableDeclaration, Method method) {
+    	//check if this is a lambda parameter
+    	AbstractCodeFragment lambdaStatement = null;
+    	for(LambdaExpressionObject lambda : method.getUmlOperation().getAllLambdas()) {
+    		if(lambda.getParameters().contains(variableDeclaration) && method.getUmlOperation().getBody() != null) {
+    			for(AbstractCodeFragment leaf : method.getUmlOperation().getBody().getCompositeStatement().getLeaves()) {
+    				if(leaf.getLambdas().contains(lambda)) {
+    					lambdaStatement = leaf;
+    					break;
+    				}
+    			}
+    			if(lambdaStatement != null) {
+    				break;
+    			}
+    		}
+    	}
         String name = String.format("%s$%s(%d)", method.getName(), variableDeclaration.toString().replace(" ", ""), variableDeclaration.getLocationInfo().getStartLine());
         String sha512 = Util.getSHA512(variableDeclaration.getScope().getStatementsInScopeUsingVariable().stream().map(AbstractCodeFragment::toString).collect(Collectors.joining()));
-        String identifierExcludeVersion = String.format(
+        String identifierExcludeVersion = null;
+        if(lambdaStatement != null) {
+        	identifierExcludeVersion = String.format(
+                "%s$%s%s:%s%s{%s,%s,%s}",
+                method.getIdentifierIgnoringVersion(),
+                variableDeclaration.isFinal() ? "(final)" : "",
+                variableDeclaration.getVariableName(),
+                //variableDeclaration.getType().toQualifiedString(),
+                variableDeclaration.getType() != null ? variableDeclaration.getType().toQualifiedString() : null,
+                Util.annotationsToString(variableDeclaration.getAnnotations()),
+                sha512,
+                variableDeclaration.getScope().getParentSignature(),
+                Util.getSHA512(lambdaStatement.getString())
+            );
+        }
+        else {
+        	identifierExcludeVersion = String.format(
                 "%s$%s%s:%s%s{%s,%s}",
                 method.getIdentifierIgnoringVersion(),
                 variableDeclaration.isFinal() ? "(final)" : "",
@@ -41,7 +73,8 @@ public class Variable extends BaseCodeElement {
                 Util.annotationsToString(variableDeclaration.getAnnotations()),
                 sha512,
                 variableDeclaration.getScope().getParentSignature()
-        );
+        	);
+        }
         return new Variable(variableDeclaration, method.getUmlOperation(), identifierExcludeVersion, name, variableDeclaration.getLocationInfo().getFilePath(), method.getVersion());
     }
 
